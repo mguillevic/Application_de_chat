1c3f384d13cd733c9b143cc3acf33bdf4a79eb0b
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2021-10-15 18:50:15 +0200

    Classes de thread pour gérer le multicast

---
+++ Code-Socket/src/controller/controller/MulticastReceiveThread.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+
+public class MulticastReceiveThread extends Thread{
+	private MulticastSocket socket;
+	
+	public MulticastReceiveThread(MulticastSocket socket) {
+		this.socket=socket;
+	}
+	public void run() {
+		try{
+			while(true) {
+				byte[] message = new byte[256];
+				DatagramPacket packet = new DatagramPacket(message, message.length);
+	        
+				// receive the packet
+				socket.receive(packet);
+				System.out.println(new String(packet.getData()));
+			}
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+}
